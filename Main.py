7debb8671341f02d6e0b087ccf6f2017b2021fdf
Joshua Garza <nikerun97@gmail.com>
2024-07-19 00:34:41 -0500

    Small update to Main.py

---
+++ Main.py
@@ -0,0 +1,6 @@
+class SteamLeakCalculator():
+    def __init__(self, PipeID, OrificeDiameter, VaporMW, Pressure):
+        self.PipeID = PipeID
+        self.OrificeDiameter = OrificeDiameter
+        self.VaporMW = VaporMW
+        self.Pressure = Pressure
